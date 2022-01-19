85c4e0078f9a5cad6c8a21c34a0cdc1b8ef3e7ee
Jason Watson <jbw@jbw.cc>
2022-01-19 21:36:33 +0000

    chore: add initial

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Configuration = void 0;
+var Configuration = /** @class */ (function () {
+    function Configuration() {
+    }
+    return Configuration;
+}());
+exports.Configuration = Configuration;
+//# sourceMappingURL=index.js.map
