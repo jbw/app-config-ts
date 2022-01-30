fc0948d24a9e831628b886687c5cb25e71f634cf
Jason Watson <jbw@jbw.cc>
2022-01-30 17:38:16 +0000

    remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var implementations_1 = require("./json/implementations");
-var jsonConfigSource = new implementations_1.JsonConfigurationProvider(new implementations_1.JsonConfigurationSource());
-jsonConfigSource.loadFile("{ \"connectionString\": \"mongodb://localhost:27017/test\" }");
-var connectionString = jsonConfigSource.get('connectionString');
-console.log(connectionString);
-//# sourceMappingURL=index.js.map
