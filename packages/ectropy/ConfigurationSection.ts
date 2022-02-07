87096b3c5a40dd6b4074d50cf3c73a7d44f7e885
Jason Watson <jbw@jbw.cc>
2022-02-07 21:27:34 +0000

    refactor: remove log

--- packages/ectropy/ConfigurationSection.ts
+++ packages/ectropy/ConfigurationSection.ts
@@ -7,7 +7,7 @@ export class ConfigurationSection implements IConfigurationSection {
   value: string | null = this.root.get(this.path);
 
   constructor(public readonly root: IConfigurationRoot, public readonly path: string) {
-    console.debug('ConfigurationSection.constructor', this.root, this.path);
+    console.debug('ConfigurationSection.constructor', this.path);
   }
 
   public get(key: string): string | null {
