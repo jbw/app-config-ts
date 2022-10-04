3c58027184a24c8d6dda16ed2828539817963fe3
Jason Watson <hi@jbw.codes>
2022-10-04 21:54:36 +0100

    chore: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,10 @@
 module.exports = {
   root: true,
-
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
   env: {
     node: true,
     jest: true,
   },
   plugins: ['@typescript-eslint'],
-
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/require-await': 'error',
-  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
 };
