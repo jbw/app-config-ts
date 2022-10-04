2a6b65fde0a200933866e9fc53ead1f13dedc012
Jason Watson <hi@jbw.codes>
2022-10-04 22:23:05 +0100

    refactor: implement mono repo builds

---
+++ packages/json/release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ['main', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    {
+      assets: ['CHANGELOG.md'],
+    },
+  ],
+};
