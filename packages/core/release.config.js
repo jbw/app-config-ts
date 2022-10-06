d5d5fe560b143c917e9216a7a8718e49eaf1250e
Jason Watson <hi@jbw.codes>
2022-10-06 11:50:50 +0100

    fix: releases fix

--- packages/core/release.config.js
+++ packages/core/release.config.js
@@ -12,7 +12,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md'],
+        assets: ['CHANGELOG.md', 'package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
@@ -23,11 +23,6 @@ module.exports = {
       },
     ],
     '@semantic-release/github',
-    [
-      '@semantic-release/npm',
-      {
-        pkgRoot: 'dist',
-      },
-    ],
+    ['@semantic-release/npm'],
   ],
 };
